4cd23ca298e52cc905d5746bb58031abdf321bbf
Guido Scialfa <dev@guidoscialfa.com>
2017-11-05 12:02:23 +0100

    Introduce Nonce Url with tests

---
+++ src/NonceUrl.php
@@ -0,0 +1,128 @@
+<?php
+/**
+ * NonceUrl
+ *
+ * @author    Guido Scialfa <dev@guidoscialfa.com>
+ * @package   nonce
+ * @copyright Copyright (c) 2017, Guido Scialfa
+ * @license   GNU General Public License, version 2
+ *
+ * Copyright (C) 2017 Guido Scialfa <dev@guidoscialfa.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+namespace Unprefix\Nonce;
+
+use function Unprefix\Nonce\Helpers\slugify;
+
+/**
+ * Class NonceUrl
+ *
+ * @since   1.0.0
+ * @package Unprefix\Nonce
+ * @author  Guido Scialfa <dev@guidoscialfa.com>
+ */
+final class NonceUrl implements NonceUrlInterface
+{
+    /**
+     * Nonce
+     *
+     * @since 1.0.0
+     *
+     * @var NonceInterface The instance of the nonce for internal use
+     */
+    private $nonce;
+
+    /**
+     * Name
+     *
+     * @since 1.0.0
+     *
+     * @var string The nonce name
+     */
+    private $name;
+
+    /**
+     * Url
+     *
+     * @since 1.0.0
+     *
+     * @var string URL to add nonce action
+     */
+    private $url;
+
+    /**
+     * Make Url
+     *
+     * @since 1.0.0
+     *
+     * @param string $actionUrl The url to add nonce action.
+     *
+     * @return void
+     */
+    private function makeUrl($actionUrl)
+    {
+        $actionUrl = str_replace('&amp;', '&', $actionUrl);
+
+        $this->url = add_query_arg($this->name, $this->nonce->nonce(), $actionUrl);
+    }
+
+    /**
+     * NonceUrl constructor
+     *
+     * @since 1.0.0
+     *
+     * @param NonceInterface $nonce The instance of the nonce for internal use.
+     * @param string         $name  The nonce name.
+     * @param string         $url   URL to add nonce action.
+     */
+    public function __construct(NonceInterface $nonce, $name, $url)
+    {
+        if (! is_string($name) || '' === $name) {
+            throw new \InvalidArgumentException(
+                'Name must be a valid string in ' . __CLASS__ . ' constructor'
+            );
+        }
+
+        if (! is_string($url) || ! filter_var($url, FILTER_VALIDATE_URL)) {
+            throw new \InvalidArgumentException(
+                'Name must be a valid url in ' . __CLASS__ . ' constructor'
+            );
+        }
+
+        $this->nonce = $nonce;
+        $this->name  = slugify($name);
+
+        // Set the url.
+        $this->makeUrl($url);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function name()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function url()
+    {
+        return esc_url($this->url);
+    }
+}
---
+++ tests/src/NonceUrlTest.php
@@ -0,0 +1,124 @@
+<?php
+/**
+ * NonceUrlTest
+ *
+ * @author    Guido Scialfa <dev@guidoscialfa.com>
+ * @package   nonce
+ * @copyright Copyright (c) 2017, Guido Scialfa
+ * @license   GNU General Public License, version 2
+ *
+ * Copyright (C) 2017 Guido Scialfa <dev@guidoscialfa.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+namespace Unprefix\Nonce\Tests\src;
+
+use Brain\Monkey\Functions;
+use Unprefix\Nonce\NonceUrl;
+use Unprefix\Nonce\Tests\NonceTestCase;
+
+/**
+ * Class NonceUrlTest
+ *
+ * @author  Guido Scialfa <dev@guidoscialfa.com>
+ * @package Unprefix\Nonce\Tests
+ */
+class NonceUrlTest extends NonceTestCase
+{
+    public function testNonceUrlInstance()
+    {
+        $nonceMock = \Mockery::mock('Unprefix\\Nonce\\NonceInterface');
+        $nonceMock->shouldReceive('nonce')
+                  ->andReturn('aa');
+
+        Functions\when('esc_url')
+            ->returnArg(1);
+        Functions\when('add_query_arg')
+            ->justReturn('http://www.validnonceurl.com?nonce_url_name=aa');
+
+        $instance = new NonceUrl(
+            $nonceMock,
+            'nonce_url_name',
+            'http://www.validnonceurl.com'
+        );
+
+        $this->assertInstanceOf('Unprefix\\Nonce\\NonceUrl', $instance);
+    }
+
+    public function provideDataForUrlParameter()
+    {
+        return array(
+            array('http://www.validnonceurl.com'),
+            array('https://www.validnonceurl.com'),
+        );
+    }
+
+    /**
+     * @dataProvider provideDataForUrlParameter
+     */
+    public function testNonceUrlParameterForConstructor($url)
+    {
+        $nonceMock = \Mockery::mock('Unprefix\\Nonce\\NonceInterface');
+        $nonceMock->shouldReceive('nonce')
+                  ->andReturn('aa');
+
+        Functions\when('esc_url')
+            ->returnArg(1);
+        Functions\when('add_query_arg')
+            ->justReturn('http://www.validnonceurl.com?nonce_url_name=aa');
+
+        $instance = new NonceUrl(
+            $nonceMock,
+            'nonce_url_name',
+            $url
+        );
+
+        $this->assertInstanceOf('Unprefix\\Nonce\\NonceUrl', $instance);
+    }
+
+    public function provideDataForNameParameter()
+    {
+        return array(
+            array(''),
+            array(1),
+            array(false),
+            array(true),
+            array([]),
+            array(new \stdClass()),
+        );
+    }
+
+    /**
+     * @dataProvider provideDataForNameParameter
+     */
+    public function testNonceUrlThrowExceptionWhenNameNotAValidString($name)
+    {
+        $this->expectException(
+            'InvalidArgumentException',
+            'Name must be a valid string in Unprefix\Nonce\NonceUrl constructor'
+        );
+
+        $nonceMock = \Mockery::mock('Unprefix\\Nonce\\NonceInterface');
+        $nonceMock->shouldReceive('nonce')
+                  ->andReturn('aa');
+
+        $instance = new NonceUrl(
+            $nonceMock,
+            $name,
+            'http://www.validnonceurl.com'
+        );
+    }
+}
